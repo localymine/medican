754cf42516b882b104fb1d044d61c765b7f5da0d
Khang.LD <leduongkhang@gmail.com>
2017-04-05 00:20:28 +0700

    delete old source

--- wp-content/themes/medican/azexo/less-variables.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (class_exists('WPLessPlugin')) {
-    $less = WPLessPlugin::getInstance();
-    $options = get_option(AZEXO_THEME_NAME);
-    if (isset($options['brand-color']))
-        $less->addVariable('brand-color', $options['brand-color']);
-}
--- wp-content/themes/medican/template-parts/header-search-toggle.php
+++
@@ -1,2 +0,0 @@
-<a href="#" class="trigger search" data-trigger-class="search-triggered" data-trigger-on="#masthead" data-trigger-off="#masthead"></a>
-
